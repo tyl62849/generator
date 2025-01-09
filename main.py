6ec09dd53f4cfc0d5e28eac05039db3d0a69a815
Tyl62849 <tyl62849@gmail.com>
2025-01-08 22:28:13 -0500

    added main.sh

---
+++ main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    node = TextNode("This is a text node", text_type = "bold", url = "https://example.com" )
+    print(node)
+
+if __name__ == "__main__":
+    main()
